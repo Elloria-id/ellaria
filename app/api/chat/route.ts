dfc960ba4c77649b99dedd7cef33cdcdda288d05
lalapiuwyzz <lalakayla11623@gmail.com>
2026-08-25 14:55:32 +0700

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { prisma } from "@/lib/prisma";
-import { authOptions } from "@/lib/auth";
-
+import { prisma } from "@/lib/db/prisma";
+import { authOptions } from "@/lib/auth/auth";
 export async function GET(req: Request) {
   try {
     const session = await getServerSession(authOptions);
